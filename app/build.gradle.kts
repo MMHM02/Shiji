79dc4381c4a941f411e579cb3a4e65e1c7c4ea41
MMHM02 <zlx13838156545@gmail.com>
2026-07-28 21:17:41 +0800

    v0.1.6 — new app icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName = "0.1.5"
+        versionName = "0.1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
